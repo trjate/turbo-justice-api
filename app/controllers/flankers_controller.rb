85395c510c406bf19ecc410244aeebcdf523e09d
Trevor Terris <tjterris@gmail.com>
2015-10-12 18:53:18 -0400

    devise, authentication and rails api changes.

--- app/controllers/flankers_controller.rb
+++ app/controllers/flankers_controller.rb
@@ -1,17 +1,26 @@
 class FlankersController < ApplicationController
     before_action :set_flanker_id, only: [:show,:destroy,:update]
-  	# before_action :authenticate_user!
+  	before_action :authenticate_user!
+    before_action :set_headers
 
   	def index
-  		# @flankers = current_user.flankers.all
-  		# render json: @flankers
-      @flankers = Flanker.all
-      render json: @flankers
+  		@flankers = current_user.flankers.all
+  		render json: @flankers
   	end
 
     def create
+
       @flanker = Flanker.new(flanker_params)
-      @flanker.user_id = current_user.id
+
+      @flanker.user_id = @current_user.id
+
+      if @flanker.save
+        render json: @flanker,
+          status: :ok
+      else
+        render json: { errors: @flanker.errors.full_messages },
+               status: :unprocessable_entity
+      end
     end
 
     def show
@@ -22,11 +31,16 @@ class FlankersController < ApplicationController
   private
 
     def flanker_params
-      params.require(:flanker).permit(:correct_guess, :incorrect_guesses)
+      params.permit(:correct_guesses, :incorrect_guesses)
     end
 
     def set_flanker_id
       @flanker = Flanker.find(params[:id])
+    end
+
+    def set_headers
+      client = response.headers['client']
+      access_token = response.headers['access_token']
     end
 
 
