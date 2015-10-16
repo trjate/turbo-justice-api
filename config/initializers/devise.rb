92fa9029e912591f97ca1b4ec821e9d003205e52
Trevor Terris <tjterris@gmail.com>
2015-10-15 21:14:09 -0400

    config updates for eb deploy.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,4 +4,6 @@ Devise.setup do |config|
   # middleware b/c rails-api does not include it.
   # See: http://stackoverflow.com/q/19600905/806956
   config.navigational_formats = [:json]
+  config.secret_key = ENV['DEVISE_KEY']
+  config.password_length = 8..72
 end
