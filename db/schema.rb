0657e15981c05dc87c98f7e7948efc42b398a616
Trevor Terris <tjterris@gmail.com>
2015-10-16 11:33:34 -0400

    added click times to flankers game, refactored and hopefully have it deployable.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015221817) do
+ActiveRecord::Schema.define(version: 20151016145045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20151015221817) do
     t.integer  "correct_guesses"
     t.integer  "incorrect_guesses"
     t.integer  "user_id"
+    t.datetime "click_times"
   end
 
   create_table "users", force: :cascade do |t|
