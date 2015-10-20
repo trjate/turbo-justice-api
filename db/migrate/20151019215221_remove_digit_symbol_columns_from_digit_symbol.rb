840b8c16429c066085c097650478acc75605c256
Trevor Terris <tjterris@gmail.com>
2015-10-19 22:31:43 -0400

    migration update

--- db/migrate/20151019215221_remove_digit_symbol_columns_from_digit_symbol.rb
+++
@@ -1,7 +0,0 @@
-class RemoveDigitSymbolColumnsFromDigitSymbol < ActiveRecord::Migration
-  def change
-    remove_column :digit_symbols, :total_correct_digit_symbol_guesses, :integer, default: 0
-    remove_column :digit_symbols, :total_incorrect_digit_symbol_guesses, :integer, default: 0
-    remove_column :digit_symbols, :digit_symbol_games_played, :integer, default: 0
-  end
-end
